9201cf443133fe6f7eaefaad6322dcb48c4034da
Nbiba Bedis <bedisnbiba@gmail.com>
2021-01-24 13:14:17 +0100

    gbar daemon

---
+++ gbar_client/src/main.rs
@@ -0,0 +1,26 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+fn main() {
+    let mut send_stream = TcpStream::connect("127.0.0.1:38451").unwrap();
+    let mut recv_stream = TcpStream::connect("127.0.0.1:38451").unwrap();
+    let mut bin = String::new();
+    let args: Vec<u8> = std::io::stdin()
+        .lock()
+        .split(b'\n')
+        .filter_map(Result::ok)
+        .map(|mut s| {
+            s.push(b'\n');
+            s
+        })
+        .flatten()
+        .collect();
+
+    send_stream.write_all(&args).unwrap();
+    send_stream.flush().unwrap();
+    drop(send_stream);
+    recv_stream.read_to_string(&mut bin).unwrap();
+    if !bin.is_empty() {
+        println!("{}", &bin);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,55 @@ use gtk::{
     ContainerExt, EditableSignals, Entry, EntryExt, GtkWindowExt, Label, LabelExt, ListBox,
     ListBoxExt, ListBoxRow, WidgetExt, Window, WindowType,
 };
-use std::io::BufRead;
+use std::cell::RefCell;
+use std::io::Read;
+use std::io::Write;
+use std::net::TcpStream;
 use std::rc::Rc;
+use std::sync::mpsc;
 const MAX_VISIBLE: usize = 10;
+const ADDRESS: &str = "127.0.0.1:38451";
 fn main() {
+    if TcpStream::connect(ADDRESS).is_ok() {
+        // gbar is already running
+        return;
+    }
+
     gtk::init().unwrap();
-    let args: Vec<String> = std::io::stdin()
-        .lock()
-        .lines()
-        .filter_map(Result::ok)
-        .collect();
-    let _bar = Bar::new(args);
+
+    let (tx, rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
+    let (tx2, rx2) = mpsc::channel();
+    start_server(tx, rx2);
+    let _bar = Bar::new(rx, tx2);
 
     gtk::main();
 }
+fn start_server(tx: glib::Sender<Vec<String>>, rx2: mpsc::Receiver<String>) {
+    std::thread::spawn(move || {
+        use std::net::TcpListener;
+        let listener = TcpListener::bind(ADDRESS).unwrap();
+        let mut listener = listener.incoming();
 
+        loop {
+            let mut recv_stream = listener.next().unwrap().unwrap();
+            let mut send_stream = listener.next().unwrap().unwrap();
+
+            let mut args = String::new();
+            recv_stream.read_to_string(&mut args).unwrap();
+            let args = args.lines().map(ToOwned::to_owned).collect();
+
+            tx.send(args).unwrap();
+            let bin = rx2.recv().unwrap();
+
+            send_stream.write_all(bin.as_bytes()).unwrap();
+            send_stream.flush().unwrap();
+        }
+    });
+}
 struct Bar {}
 
 impl Bar {
-    fn new(args: Vec<String>) -> Self {
+    fn new(rx: glib::Receiver<Vec<String>>, tx2: mpsc::Sender<String>) -> Self {
         let win = Window::new(WindowType::Toplevel);
         win.set_default_size(400, 300);
         let vbox = gtk::Box::new(gtk::Orientation::Vertical, 10);
@@ -30,11 +60,8 @@ impl Bar {
         let fuzzy_c = fuzzy.clone();
         let fuzzy_c2 = fuzzy.clone();
 
-        let args = Rc::new(args);
+        let args: Rc<RefCell<Vec<String>>> = Rc::new(RefCell::new(vec![]));
         let args_c = args.clone();
-        args.iter().take(MAX_VISIBLE).for_each(|a| {
-            fuzzy.add(&Label::new(Some(&a)));
-        });
 
         let input = Entry::new();
         input.connect_changed(move |ent| {
@@ -43,6 +70,7 @@ impl Bar {
                 fuzzy_c.remove(c);
             });
             args_c
+                .borrow()
                 .iter()
                 .filter(|a| a.contains(&text))
                 .take(MAX_VISIBLE)
@@ -60,20 +88,38 @@ impl Bar {
                     .unwrap(),
             ));
         });
-        win.connect_key_press_event(move |_, key| match key.get_keyval() {
+
+        let win_c = win.clone();
+        let input_c = input.clone();
+        let fuzzy_c = fuzzy.clone();
+        let hide = move || {
+            win_c.hide();
+            input_c.set_text("");
+            fuzzy_c.get_children().iter().for_each(|c| {
+                fuzzy_c.remove(c);
+            });
+        };
+
+        win.connect_key_press_event(move |_win, key| match key.get_keyval() {
             gdk::keys::constants::Escape => {
-                gtk::main_quit();
+                tx2.send("".into()).unwrap();
+                //gtk::main_quit();
+                hide();
                 gtk::Inhibit(false)
             }
             gdk::keys::constants::Return => {
-                let bin: Label = fuzzy_c2.get_selected_row().unwrap().get_children()[0]
-                    .clone()
-                    .downcast()
-                    .unwrap();
-                //let _ = std::process::Command::new(bin.get_text().to_string()).spawn();
-                println!("{}", bin.get_text());
+                if !fuzzy_c2.get_children().is_empty() {
+                    let bin: Label = fuzzy_c2.get_selected_row().unwrap().get_children()[0]
+                        .clone()
+                        .downcast()
+                        .unwrap();
+                    //let _ = std::process::Command::new(bin.get_text().to_string()).spawn();
+                    //println!("{}", bin.get_text());
+                    tx2.send(bin.get_text().to_string()).unwrap();
+                }
 
-                gtk::main_quit();
+                //gtk::main_quit();
+                hide();
                 gtk::Inhibit(false)
             }
             _ => gtk::Inhibit(false),
@@ -82,13 +128,13 @@ impl Bar {
         vbox.add(&input);
         vbox.add(&fuzzy);
         win.add(&vbox);
-        win.show_all();
-        fuzzy.select_row(Some(
-            &fuzzy.get_children()[0]
-                .clone()
-                .downcast::<ListBoxRow>()
-                .unwrap(),
-        ));
+        rx.attach(None, move |new_args| {
+            let mut args = args.borrow_mut();
+            args.clear();
+            *args = new_args;
+            win.show_all();
+            glib::Continue(true)
+        });
         Self {}
     }
 }
