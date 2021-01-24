df3e3132e0e3befa1ade4c19db1c496c89cd1ca7
Nbiba Bedis <bedisnbiba@gmail.com>
2021-01-24 18:48:56 +0100

    clean up server code

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,14 @@ use std::io::Write;
 use std::net::TcpStream;
 use std::rc::Rc;
 use std::sync::mpsc;
+
 const MAX_VISIBLE: usize = 10;
 const ADDRESS: &str = "127.0.0.1:38451";
+
+macro_rules! shadow_clone {
+       ($($i:ident),+) => ($(let $i = $i.clone();)+)
+   }
+
 fn main() {
     if TcpStream::connect(ADDRESS).is_ok() {
         // gbar is already running
@@ -22,10 +28,11 @@ fn main() {
     let (tx, rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
     let (tx2, rx2) = mpsc::channel();
     start_server(tx, rx2);
-    let _bar = Bar::new(rx, tx2);
+    start_gui(rx, tx2);
 
     gtk::main();
 }
+
 fn start_server(tx: glib::Sender<Vec<String>>, rx2: mpsc::Receiver<String>) {
     std::thread::spawn(move || {
         use std::net::TcpListener;
@@ -51,106 +58,103 @@ fn start_server(tx: glib::Sender<Vec<String>>, rx2: mpsc::Receiver<String>) {
         }
     });
 }
-struct Bar {}
 
-impl Bar {
-    fn new(rx: glib::Receiver<Vec<String>>, tx2: mpsc::Sender<String>) -> Self {
-        let win = Window::new(WindowType::Toplevel);
-        win.set_default_size(400, 300);
-        let vbox = gtk::Box::new(gtk::Orientation::Vertical, 10);
+fn start_gui(rx: glib::Receiver<Vec<String>>, tx2: mpsc::Sender<String>) {
+    let args: Rc<RefCell<Vec<String>>> = Rc::new(RefCell::new(vec![]));
 
-        let fuzzy = ListBox::new();
-        let fuzzy_c = fuzzy.clone();
-        let fuzzy_c2 = fuzzy.clone();
-        let fuzzy_c3 = fuzzy.clone();
+    let win = Window::new(WindowType::Toplevel);
+    win.set_default_size(400, 300);
 
-        let args: Rc<RefCell<Vec<String>>> = Rc::new(RefCell::new(vec![]));
-        let args_c = args.clone();
+    let vbox = gtk::Box::new(gtk::Orientation::Vertical, 10);
+    let input = Entry::new();
+    let fuzzy = ListBox::new();
 
-        let input = Entry::new();
+    {
+        shadow_clone!(fuzzy, args, input);
         input.connect_changed(move |ent| {
             let text = ent.get_text().to_string();
-            fuzzy_c.get_children().iter().for_each(|c| {
-                fuzzy_c.remove(c);
+            fuzzy.get_children().iter().for_each(|c| {
+                fuzzy.remove(c);
             });
-            args_c
-                .borrow()
+            args.borrow()
                 .iter()
                 .filter(|a| a.contains(&text))
                 .take(MAX_VISIBLE)
                 .for_each(|a| {
-                    fuzzy_c.add(&Label::new(Some(&a)));
+                    fuzzy.add(&Label::new(Some(&a)));
                 });
-            fuzzy_c.show_all();
-            if fuzzy_c.get_children().is_empty() {
+            fuzzy.show_all();
+            if fuzzy.get_children().is_empty() {
                 return;
             }
-            fuzzy_c.select_row(Some(
-                &fuzzy_c.get_children()[0]
+            fuzzy.select_row(Some(
+                &fuzzy.get_children()[0]
                     .clone()
                     .downcast::<ListBoxRow>()
                     .unwrap(),
             ));
         });
+    }
 
-        let win_c = win.clone();
-        let input_c = input.clone();
-        let fuzzy_c = fuzzy.clone();
-        let hide = move || {
-            win_c.hide();
-            input_c.set_text("");
-            fuzzy_c.get_children().iter().for_each(|c| {
-                fuzzy_c.remove(c);
+    let hide = {
+        shadow_clone!(win, input, fuzzy);
+        move || {
+            win.hide();
+            input.set_text("");
+            fuzzy.get_children().iter().for_each(|c| {
+                fuzzy.remove(c);
             });
-        };
+        }
+    };
 
-        let tx2_c = tx2.clone();
-        let hide_c = hide.clone();
+    {
+        shadow_clone!(win, tx2, hide);
         win.connect_delete_event(move |_, _| {
-            tx2_c.send("".into()).unwrap();
-            //gtk::main_quit();
-            hide_c();
+            tx2.send("".into()).unwrap();
+            hide();
             gtk::Inhibit(true)
         });
+    }
+
+    {
+        shadow_clone!(fuzzy);
         win.connect_key_press_event(move |_win, key| match key.get_keyval() {
             gdk::keys::constants::Escape => {
                 tx2.send("".into()).unwrap();
-                //gtk::main_quit();
                 hide();
                 gtk::Inhibit(false)
             }
             gdk::keys::constants::Return => {
-                if !fuzzy_c2.get_children().is_empty() {
-                    let bin: Label = fuzzy_c2.get_selected_row().unwrap().get_children()[0]
+                if !fuzzy.get_children().is_empty() {
+                    let bin: Label = fuzzy.get_selected_row().unwrap().get_children()[0]
                         .clone()
                         .downcast()
                         .unwrap();
-                    //let _ = std::process::Command::new(bin.get_text().to_string()).spawn();
-                    //println!("{}", bin.get_text());
                     tx2.send(bin.get_text().to_string()).unwrap();
                 } else {
                     tx2.send("".into()).unwrap();
                 }
-
-                //gtk::main_quit();
                 hide();
                 gtk::Inhibit(false)
             }
             _ => gtk::Inhibit(false),
         });
+    }
 
-        vbox.add(&input);
-        vbox.add(&fuzzy);
-        win.add(&vbox);
+    vbox.add(&input);
+    vbox.add(&fuzzy);
+    win.add(&vbox);
+
+    {
+        shadow_clone!(win, fuzzy);
         rx.attach(None, move |new_args| {
             let mut args = args.borrow_mut();
-            args.clear();
             *args = new_args;
             args.iter().take(MAX_VISIBLE).for_each(|a| {
-                fuzzy_c3.add(&Label::new(Some(&a)));
+                fuzzy.add(&Label::new(Some(&a)));
             });
-            fuzzy_c3.select_row(Some(
-                &fuzzy_c3.get_children()[0]
+            fuzzy.select_row(Some(
+                &fuzzy.get_children()[0]
                     .clone()
                     .downcast::<ListBoxRow>()
                     .unwrap(),
@@ -158,6 +162,5 @@ impl Bar {
             win.show_all();
             glib::Continue(true)
         });
-        Self {}
     }
 }
